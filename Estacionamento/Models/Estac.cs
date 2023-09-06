f1aa10b2c6b792e5adcc9f3073874a210184ba95
rodrigozoran <rodrigozoran@gmail.com>
2023-09-06 12:11:41 -0300

    Adicionar arquivos de projeto.

---
+++ Estacionamento/Models/Estac.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Models
+{
+    internal class Estac
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 5;
+        private decimal valorTotal = 0;
+        private decimal estadia = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estac(decimal precoInicial, decimal precoHora, decimal valorTotal) { 
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+            this.valorTotal = 0;
+        }
+
+        public void AdicionarVeiuclo()
+        {
+            Console.WriteLine("Digite a placa para adicionar o veículo: ");
+            string? vehs = Console.ReadLine();
+
+            veiculos.Add(vehs);
+            Console.WriteLine("Veículo de placa " + vehs + " adicionado com sucesso.");
+        }
+
+        public void ListarPorPlaca()
+        {
+            Console.WriteLine("Consulte um veículo pela placa: ");
+            string? vehsC = Console.ReadLine();
+
+            bool ok = false;
+
+            foreach(string placa in veiculos)
+            {
+                if(placa.Equals(vehsC, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Placa {placa}, encontrada no sistema");
+                    ok = true;
+                    break;
+                }
+            }
+
+            if(!ok) {
+                Console.WriteLine($"Placa {vehsC} não cadastrada no sistema");
+            }
+        }
+
+        public void ListarTodos()
+        {
+            if(veiculos.Count > 0)
+            {
+                Console.WriteLine("Lista de Veículos Cadastrados: \n");
+                foreach (string placa in veiculos)
+                {
+                    Console.WriteLine($"Placa: {placa}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum veículo no sistema.");
+            }
+        }
+
+        public void RemoverVeiculos()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Contains(placa))
+            {
+                Console.WriteLine($"Encontrado: {placa}.");
+                veiculos.Remove(placa);
+                Console.WriteLine($"Veículo de placa {placa} removido com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine($"Nenhum veículo de placa {placa} cadastrado no sistema.");
+            }
+        }
+
+
+        public void totalPg()
+        {
+            Console.WriteLine("Consulte um veículo pela placa: ");
+            string? vehsC = Console.ReadLine();
+
+            bool ok = false;
+
+            foreach (string placa in veiculos)
+            {
+                if (placa.Equals(vehsC, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Placa {placa}, encontrada no sistema");
+                    ok = true;
+                    break;
+                }
+            }
+
+            if (!ok)
+            {
+                Console.WriteLine($"Placa {vehsC} não cadastrada no sistema");
+            }
+            else
+            {
+                Console.WriteLine("Tempo estacionado: ");
+                string estd = Console.ReadLine();
+
+                if(decimal.TryParse(estd, out decimal estadia))
+                {
+                    precoHora = 5;
+                    Console.WriteLine($"Tempo de Estadia no estacionamento: {estadia} horas");
+                    valorTotal = (precoHora * estadia);
+                    Console.WriteLine($"Veículo de placa {vehsC}, precisa pagar R$ {valorTotal}");
+                }
+                else
+                {
+                    Console.WriteLine("Tempo inválido.");
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Estacionamento/Program.cs
@@ -0,0 +1,62 @@
+﻿using Estacionamento.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoHora = 5;
+decimal estadia = 0;
+decimal valorTotal = 0;
+
+Console.WriteLine("Bem vindo ao Estacionamento!\nDigite o tempo de estadia: ");
+
+estadia = Convert.ToDecimal(estadia);
+
+Console.WriteLine("Tempo de Estadia: " + estadia);
+
+valorTotal = Convert.ToDecimal(precoHora*estadia);
+
+Estac es = new Estac(precoHora,estadia,valorTotal);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar por placa");
+    Console.WriteLine("4 - Listar todos os veículos");
+    Console.WriteLine("5 - Valor a pagar");
+    Console.WriteLine("6 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiuclo();
+            break;
+        case "2":
+            es.RemoverVeiculos();
+            break;
+        case "3":
+            es.ListarPorPlaca();
+            break;
+        case "4":
+            es.ListarTodos();
+            break;
+        case "5":
+            es.totalPg();
+            break;
+        case "6":
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção Inválida!");
+            break;
+    }
+
+    Console.WriteLine("Pressione alguma tecla para continuar...");
+    Console.ReadLine();
+}
+
+Console.WriteLine("Fim da Execução do Programa.");
